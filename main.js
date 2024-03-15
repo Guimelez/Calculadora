c06cc06513cdd0fc755c53fea4457fdf412a06cd
Gᴜɪᴍᴇʟᴇᴢ ツ <106618571+Guimelez@users.noreply.github.com>
2024-03-15 18:11:12 -0300

    akajsb

---
+++ main.js
@@ -0,0 +1,11 @@
+function loginAsAdmin() {
+                          var senha = prompt("Digite a senha de administrador:");
+
+                                                  if (senha === "galeto") {
+                                                                              document.getElementById("adminPanel").style.display = "block";
+                                                                                                            } else {
+                                                                                                                                              alert("Senha incorreta. Acesso negado.");
+                                                                                                                                                                                  }
+                                                                                                                                                                                                                      }
+                                                                                                                                                                                                                                                          
+
